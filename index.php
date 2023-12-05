e589f7ea913a3b7759f526453cdf19e2ecd7554e
Atteneri Estévez <atteeste2003@gmail.com>
2023-12-05 20:04:46 +0000

    orderBy and session_start

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
+<?php
+// $sort = isset($_GET['sort']) ? $_GET['sort'] : isset($_COOKIE["sort"]) ? $_COOKIE['sort'] :"id";
+if(isset($_GET['sort'])){
+  $sort = $_GET['sort'];
+} else if ( isset($_COOKIE["sort"])){
+  $sort = $_COOKIE['sort'];
+} else {
+  $sort = "id";
+}
+setcookie('sort', $sort);
+
+// Iniciamos la sesión o recuperamos la anterior existente
+session_start();
+// Comprobamos si la variable existe
+if (!isset($_SESSION['stock'])){
+  $_SESSION['stock']=0;
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Tienda</title>
   <style>
    table, td, th {
   border: 1px solid;
@@ -17,10 +37,16 @@ table {
   width: 70%;
   border-collapse: collapse;
 }
+/* 
+input {
+  border: none;
+  background: none;
+} */
   </style>
 </head>
 <body>
   <h2>Tienda</h2>
+  <h3>Carrito: <?=$_SESSION['stock']?></h3>
 
     <table>
     <tr>
@@ -28,14 +54,12 @@ table {
       <th><a href="?sort=name">Nombre</a></th>
       <th><a href="?sort=price">Precio</a></th>
       <th><a href="?sort=stock">Stock</a></th>
+      <th> </th>
+
     </tr>
      <tr>
 
 <?php
-
-$sort = isset($_GET['sort']) ? $_GET['sort'] : "id";
-setcookie('sort', $sort);
-
 
 orderBy($sort);
 
@@ -69,7 +93,10 @@ function orderBy($sort){
    <td id = "<?=$id?>"><?=$id?></td>
    <td id = "<?=$id?>"><?=$name?></td>
    <td id = "<?=$id?>"><?=$price?></td>
-   <td id = "<?=$id?>"> <?=$amount?></td>
+   <td id = "<?=$id?>"><?=$amount?></td>
+   <!-- No funciona el carrito -->
+   <td><input type="button" name="add_cart_button" value="&#128722;" onclick="location.href='addCart.php'"></td> 
+   <!-- <a href="prueba.php"><span style='font-size:20px;'>&#128722;</span></a> -->
  <?php
    $field = $result->fetch_assoc();
    ?>
@@ -79,6 +106,12 @@ function orderBy($sort){
  $result->close();
 
 }
+
+function addCart() {
+  $_SESSION['stock']++;
+
+}
+
 ?> 
      
      </table>
